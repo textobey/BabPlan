9082dcad0acbe1d8fc5e60517a736307e6a4dfa3
textobey <dogsan_e@naver.com>
2022-06-14 15:06:25 +0900

    프로젝트 생성

---
+++ BabPlan/BabPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabPlanApp.swift
+//  BabPlan
+//
+//  Created by 이서준 on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct BabPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabPlan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BabPlan
+//
+//  Created by 이서준 on 2022/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
